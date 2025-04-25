c53ab870632aa460717c29a6ab828b87b10520cf
Mücahit Buğday <mucahit.bugday@outlook.com>
2025-04-26 01:52:35 +0300

    ...

--- src/App.tsx
+++ src/App.tsx
@@ -1,25 +1,24 @@
-import { StyleSheet, Text, View } from 'react-native'
 import React from 'react'
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import { NavigationContainer } from '@react-navigation/native';
 
-
 import OnboardingScreen from './screens/OnboardingScreen';
 import ChatListScreen from './screens/ChatListScreen';
 import ChatDetailScreen from './screens/ChatDetailScreen';
+import DiscoverScreen from './screens/DiscoverScreen';
 
 export default function App() {
   const Stack = createNativeStackNavigator();
 
   return (
     <NavigationContainer>
-    <Stack.Navigator initialRouteName="OnboardingScreen">
-      <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="ChatListScreen" component={ChatListScreen} options={{ headerShown: false }} />
-      <Stack.Screen name="ChatDetailScreen" component={ChatDetailScreen} options={{ headerShown: false }} />
-    </Stack.Navigator>
-  </NavigationContainer>
+      <Stack.Navigator initialRouteName="OnboardingScreen">
+        <Stack.Screen name="OnboardingScreen" component={OnboardingScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ChatListScreen" component={ChatListScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="ChatDetailScreen" component={ChatDetailScreen} options={{ headerShown: false }} />
+        <Stack.Screen name="DiscoverScreen" component={DiscoverScreen} options={{ headerShown: false }} />
+      </Stack.Navigator>
+    </NavigationContainer>
   )
 }
 
-const styles = StyleSheet.create({})
